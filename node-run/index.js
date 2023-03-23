2f9c26481ee7ca458b66677cdefac36ee892f354
sunlee <hildxd@outlook.com>
2023-03-23 09:36:54 +0800

    fix: node example

--- node-run/index.js
+++ node-run/index.js
@@ -1,24 +1,29 @@
 
 
 import fs from "fs";
-import { init, WASI } from "@wasmer/wasi";
-
-// This is needed to load the WASI library first
-await init();
-
-let wasi = new WASI({
-  env: {},
-  args: [],
-});
 
 const buf = fs.readFileSync('../wasm_pest.wasm');
+const wasmModule = new WebAssembly.Module(buf)
+const instance = new WebAssembly.Instance(wasmModule)
+console.log(instance.exports.sum(1, 3))
+// import { init, WASI } from "@wasmer/wasi";
 
-const module = await WebAssembly.compile(
-  new Uint8Array(buf)
-);
-console.log(await wasi.instantiate(module, {}).exports.sum(1, 3))
+// This is needed to load the WASI library first
+// await init();
 
-let exitCode = wasi.start();
-let stdout = wasi.getStdoutString();
-// This should print "hello world (exit code: 0)"
-console.log(`${stdout}(exit code: ${exitCode})`);
+// let wasi = new WASI({
+//   env: {},
+//   args: [],
+// });
+
+// const buf = fs.readFileSync('../wasm_pest.wasm');
+
+// const module = await WebAssembly.compile(
+//   new Uint8Array(buf)
+// );
+// console.log(await wasi.instantiate(module, {}).exports.sum(1, 3))
+
+// let exitCode = wasi.start();
+// let stdout = wasi.getStdoutString();
+// // This should print "hello world (exit code: 0)"
+// console.log(`${stdout}(exit code: ${exitCode})`);
